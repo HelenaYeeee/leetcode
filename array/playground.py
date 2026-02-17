66a482a5878ee3f2bad17bd5321518b3cc3aea9d
Helinia <helena.xinming.ye@gmail.com>
2026-02-17 15:29:31 -0500

    test1

--- array/playground.py
+++ array/playground.py
@@ -1,3 +1,4 @@
 test = {1:2, 3:5, 5:2}
 element_counter = sorted(test.items(), key=lambda x: (x[1], x[0]), reverse = True)
-print(dict(element_counter)) 
+print(dict(element_counter))    
+print(test)
